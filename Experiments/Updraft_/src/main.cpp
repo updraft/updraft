59309919711f12f5dbabe92c5962607edd700015
Cestmir Houska <czestmyr@gmail.com>
2011-11-30 14:34:36 +0100

    Updraft_ experiment: Added Qt Linguist support
    Signed-off-by: Cestmir Houska <czestmyr@gmail.com>

--- Experiments/Updraft_/src/main.cpp
+++ Experiments/Updraft_/src/main.cpp
@@ -1,8 +1,14 @@
 #include <QtGui/QApplication>
+#include <QTranslator>
 #include "ui/mainwindow.h"
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+
+    QTranslator trans;
+    trans.load("czech");
+    a.installTranslator(&trans);
+
     MainWindow w;
     w.show();
 
