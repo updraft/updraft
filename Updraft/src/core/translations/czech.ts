61120b9cc41a44e3b8e832dcc1bf506bf30349cd
Cestmir Houska <czestmyr@gmail.com>
2011-12-07 19:32:49 +0100

    CMakeLists: Automated *.ts translation file generation
    Signed-off-by: Cestmir Houska <czestmyr@gmail.com>

--- Updraft/src/core/translations/czech.ts
+++ Updraft/src/core/translations/czech.ts
@@ -1,42 +1,80 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="cs_CZ" sourcelanguage="en">
+<TS version="2.0" language="cs" sourcelanguage="en">
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="20"/>
+        <location filename="../ui/mainwindow.ui" line="20"/>
         <source>Updraft</source>
-        <translation type="unfinished">Stoupák</translation>
+        <translation>Stoupák</translation>
     </message>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="104"/>
+        <location filename="../ui/mainwindow.ui" line="99"/>
         <source>flight.igc</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="114"/>
+        <location filename="../ui/mainwindow.ui" line="109"/>
         <source>task.cup</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="134"/>
+        <location filename="../ui/mainwindow.ui" line="129"/>
         <source>File</source>
-        <translation type="unfinished">Soubor</translation>
+        <translation>Soubor</translation>
     </message>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="139"/>
+        <location filename="../ui/mainwindow.ui" line="134"/>
         <source>View</source>
-        <translation type="unfinished">Pohled</translation>
+        <translation>Pohled</translation>
     </message>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="144"/>
+        <location filename="../ui/mainwindow.ui" line="139"/>
         <source>Map</source>
-        <translation type="unfinished">Mapa</translation>
+        <translation>Mapa</translation>
     </message>
     <message>
-        <location filename="src/ui/mainwindow.ui" line="149"/>
+        <location filename="../ui/mainwindow.ui" line="144"/>
         <source>Help</source>
-        <translation type="unfinished">Nápověda</translation>
+        <translation>Nápověda</translation>
+    </message>
+</context>
+<context>
+    <name>Updraft::Core::PluginManager</name>
+    <message>
+        <location filename="../pluginmanager.cpp" line="13"/>
+        <source>Searching for plugins in plugin directory.</source>
+        <translation>Hledám moduly v adresáři plugins.</translation>
+    </message>
+    <message>
+        <location filename="../pluginmanager.cpp" line="17"/>
+        <source>Could not find &apos;plugins&apos; directory</source>
+        <translation>Nemohu najít adresář &apos;plugins&apos;</translation>
+    </message>
+    <message>
+        <location filename="../pluginmanager.cpp" line="47"/>
+        <source>Loading plugin %s</source>
+        <translation>Načítám modul %s</translation>
+    </message>
+    <message>
+        <location filename="../pluginmanager.cpp" line="88"/>
+        <source>Loading plugin failed.</source>
+        <translation>Načítání modulu selhalo.</translation>
+    </message>
+    <message>
+        <location filename="../pluginmanager.cpp" line="96"/>
+        <source>Plugin doesn&apos;t have correct interface.</source>
+        <translation>Modul nemá správné rozhraní.</translation>
+    </message>
+    <message>
+        <location filename="../pluginmanager.cpp" line="103"/>
+        <source>Wrong version of plugin API (expected %d, got %d).</source>
+        <translation>Špatná verze API (očekávám %d, dostal jsem %d).</translation>
+    </message>
+    <message>
+        <location filename="../pluginmanager.cpp" line="109"/>
+        <source>Plugin named %s already loaded</source>
+        <translation>Modul s názvem %s už byl nahrán</translation>
     </message>
 </context>
 </TS>
