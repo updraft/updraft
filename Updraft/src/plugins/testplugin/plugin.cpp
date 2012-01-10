fa817a8dc877f483d2955ffc800188c0c372dbc6
Cestmir Houska <czestmyr@gmail.com>
2012-01-10 01:03:17 +0100

    Testplugin connects to a setting's valueChanged signal
    Signed-off-by: Cestmir Houska <czestmyr@gmail.com>

--- Updraft/src/plugins/testplugin/plugin.cpp
+++ Updraft/src/plugins/testplugin/plugin.cpp
@@ -73,6 +73,8 @@ void TestPlugin::initialize() {
   testSetting2 = core->addSetting("testplugin:testsetting2", "Number of silver squirters", 333);
   testSetting3 = core->addSetting("testplugin:testsetting3", "Name of the application", "Updraft");
 
+  connect(testSetting, SIGNAL(valueChanged()), this, SLOT(showPi()));
+
   qDebug("testplugin loaded");
 }
 
