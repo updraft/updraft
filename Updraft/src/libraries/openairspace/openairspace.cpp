d74d9497ca3fcdd8508f2228dfeb5f4f696b34cb
Monkeyonagazeboo <ales.zita@volny.cz>
2012-01-22 11:58:37 +0100

    Work on Airspaces - working parser dll

--- Updraft/src/libraries/openairspace/openairspace.cpp
+++ Updraft/src/libraries/openairspace/openairspace.cpp
@@ -2,12 +2,40 @@
 
 #include <QDebug>
 #include <QFile>
+#include <QString>
 
-void TTT() {
-  qDebug("TTT");
-}
+
 namespace OpenAirspace {
-  void Parser::Test() {
-    qDebug("dll test");
+  Parser::Parser(const QString& fileName) {
+    qDebug("Parser ctor");
+    QFile file(fileName);
+
+    if (!file.open(QFile::ReadOnly)) {
+      qDebug("OpenAirspace file not found : ");
+      return;
+    }
+
+    QTextStream ts(&file);
+    QString text("");
+
+    while (text != "AC" && !ts.atEnd())
+      ts >> text;
+
+    if (ts.atEnd())
+      qDebug("Not supported OpenAirspace format.");
+    else
+      qDebug("Parsing OpenAirspace file.");
+
+    while (!ts.atEnd()) {
+      if (!ts.atEnd())
+        ts.seek(ts.pos() -3);
+      Airspace nextairspace(&ts);
+      this->allAirspaces.push_back(nextairspace);
+    }
+
+    file.close();
+  }
+  Parser::~Parser(void) {
+    qDebug("Parser dtor");
   }
 }  // OpenAirspace
--- Updraft/src/libraries/openairspace/openairspace.h
+++ Updraft/src/libraries/openairspace/openairspace.h
@@ -1,22 +1,65 @@
 #ifndef UPDRAFT_SRC_LIBRARIES_OPENAIRSPACE_OPENAIRSPACE_H_
 #define UPDRAFT_SRC_LIBRARIES_OPENAIRSPACE_OPENAIRSPACE_H_
 
-#include <QtCore/QtGlobal>
-#include <QString>
-#include <QList>
-
-#ifdef OPENAIRSPACE_LIB
-# define OPENAIRSPACE_EXPORT Q_DECL_EXPORT
-#else
-# define OPENAIRSPACE_EXPORT Q_DECL_IMPORT
-#endif
-
-void OPENAIRSPACE_EXPORT TTT();
+#include "airspace.h"
 
 namespace OpenAirspace {
   class OPENAIRSPACE_EXPORT Parser {
  public:
-    void Test();
+    /// OpenAirspace Parser Ctor
+    /// Parses the \param OpenAirspace file
+    explicit Parser(const QString&);
+
+    /// Returns the name of the AirSpace
+    inline const QString& GetName(int i) {
+      return this->allAirspaces[i].GetName();}
+
+    /// Returns the used airspaces count
+    inline size_t size() {
+      return this->allAirspaces.size(); }
+
+    /// Returns the class of the AirSpace
+    inline const int GetClass(int i) {
+      return this->allAirspaces[i].GetClass();}
+
+    /// Returns the floor of the AirSpace
+    inline const QString& GetFloor(int i) {
+      return this->allAirspaces[i].GetFloor(); }
+
+    /// Returns the ceiling of the AirSpace
+    inline const QString& GetCeiling(int i) {
+      return this->allAirspaces[i].GetCeiling(); }
+
+    /// Returns the tag coordinates of the AirSpace
+    inline const
+      QList<OpenAirspace::Airspace::Coordinate>& GetTagCoor(int i) {
+      return this->allAirspaces[i].GetTagCoor(); }
+
+    /// Returns the arcs type I
+    inline const
+      QList<OpenAirspace::Airspace::ArcI>& GetArcI(int i) {
+      return this->allAirspaces[i].GetArcI(); }
+
+    /// Returns the arcs type I
+    inline const
+      QList<OpenAirspace::Airspace::ArcII>& GetArcII(int i) {
+      return this->allAirspaces[i].GetArcII(); }
+
+    /// Returns the circles
+    inline const
+      QList<OpenAirspace::Airspace::Circle>& GetCircle(int i) {
+      return this->allAirspaces[i].GetCircle(); }
+
+    /// Returns the AirWay
+    inline const
+      QList<OpenAirspace::Airspace::Coordinate>& GetAirWay(int i) {
+      return this->allAirspaces[i].GetAirWay(); }
+
+    ~Parser(void);
+
+ private:
+    /// OpenAirspace contains several airspaces.
+    QList<Airspace> allAirspaces;
   };  // Parser
 }  // OpenAirspace
 #endif  // UPDRAFT_SRC_LIBRARIES_OPENAIRSPACE_OPENAIRSPACE_H_
