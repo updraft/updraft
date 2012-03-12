4dd6b67c0937c8ca40ee272cf72a6a8002d206f7
maria <maria.vamosova@gmail.com>
2012-03-12 12:10:50 +0100

    Work on graph plotting.

--- Updraft/src/plugins/igcviewer/plot.cpp
+++ Updraft/src/plugins/igcviewer/plot.cpp
@@ -1,16 +1,35 @@
 #include "plot.h"
 
+#include <QPoint>
+#include <QPen>
+
 namespace Updraft {
 namespace IgcViewer {
 
 
 
 void PlotWidget::paintEvent(QPaintEvent* paintEvent) {
+  QPen pen;
+  QColor color;
+
+  QPen axisPen(axisColor);
+
+  int lengthX = width() - 2*pixelOffsetX;
+  int lengthY = height() - 2*pixelOffsetY;
+
+  QPoint zero;
+  QPoint maximum;
+
+  // fill background
+
   // draw the altitude graph
   // draw axes
-  double scale = 1;  //TODO(Maria): init scale
+  zero.setX(pixelOffsetX);
+  zero.setY(pixelOffsetY + 0.25*lengthY);
+  maximum.setX(pixelOffsetX + lengthX);
+  maximum.setY(pixelOffsetY);
 
-
+  // for (int i=1; i<=altitudeInfo->fixList->size(); )
 }
 
 }  // End namespace IgcViewer
--- Updraft/src/plugins/igcviewer/plot.h
+++ Updraft/src/plugins/igcviewer/plot.h
@@ -9,7 +9,21 @@ namespace IgcViewer {
 class PlotWidget : public QWidget {
  private:
   void paintEvent(QPaintEvent* paintEvent);
-}
+
+  /// Intervals in which to draw a mark in the time axis.
+  QList<int> timeIntervals;
+
+  /// Minimum number of pixels between the marks.
+  int minPixels;
+
+  /// offset from the window border
+  static const int pixelOffsetX = 5;
+  static const int pixelOffsetY = 5;
+
+  /// drawing options
+  QColor background;
+  QColor axisColor;
+};
 
 }  // End namespace Updraft
 }  // End namespace IgcViewer
