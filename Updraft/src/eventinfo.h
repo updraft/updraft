48f9f1c3c97a1636daebff8d55f63f942ddc8cf1
Cestmir Houska <czestmyr@gmail.com>
2012-03-15 21:14:28 +0100

    Documented EventInfo
    Signed-off-by: Cestmir Houska <czestmyr@gmail.com>

--- Updraft/src/eventinfo.h
+++ Updraft/src/eventinfo.h
@@ -9,6 +9,11 @@ enum EventType {
   RIGHT_CLICK
 };
 
+/// This class holds information about a MapObject event.
+///
+/// This event happens when e.g. the user clicks a MapObject. Subsequently,
+/// all the plugins are informed about the event using this class. Everything
+/// that the plugins might need to know about the event should go here.
 class EventInfo {
  public:
   EventInfo(EventType type_): type(type_) {}
