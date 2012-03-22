b85b9a152df1bcf3da57b0a9e209f583ca7c7619
Tomas Zamecnik <pulcik@email.cz>
2012-03-22 15:36:52 +0100

    Create taskdecl static GUI
    * Create layer, handle opening/closing
    * Add static GUI to bottom panel
    * Add item to left pane
    * Add menu item for new task

--- Updraft/src/plugins/taskdecl/taskdeclaration.cpp
+++ Updraft/src/plugins/taskdecl/taskdeclaration.cpp
@@ -1,7 +1,14 @@
 #include "taskdeclaration.h"
 
 namespace Updraft {
-namespace Core {
+
+TaskDeclaration::TaskDeclaration()
+  : createTaskAction(NULL), mapLayerGroup(NULL) {
+}
+
+TaskDeclaration::~TaskDeclaration() {
+  unloadFiles();
+}
 
 QString TaskDeclaration::getName() {
   return QString("taskdecl");
@@ -12,15 +19,80 @@ unsigned TaskDeclaration::getPriority() {
 }
 
 void TaskDeclaration::initialize() {
+  mapLayerGroup = core->createMapLayerGroup(tr("Tasks"));
+
+  createTaskAction = new QAction("New Task", this);
+  connect(createTaskAction, SIGNAL(triggered()), this, SLOT(createTask()));
+
+  core->getSystemMenu(MENU_FILE)->insertAction(1, createTaskAction);
+
   qDebug("taskdecl loaded");
 }
 
 void TaskDeclaration::deinitialize() {
+  unloadFiles();
   qDebug("taskdecl unloaded");
+}
+
+bool TaskDeclaration::fileOpen(const QString &filename, int roleId) {
+  TaskFile *file = NULL;
+
+  switch (roleId) {
+    case OPEN_TASK_FILE:
+      // TODO(Tom): Implement file loading.
+      // file = TPFileCupAdapter::load(filename);
+      break;
+  }
+
+  if (file != NULL) {
+    addLayer(file);
+    return true;
+  }
+
+  return false;
+}
+
+void TaskDeclaration::fileIdentification(QStringList *roles,
+    QString *importDirectory, const QString &filename) {
+  // TODO(Tom): Implement
+}
+
+void TaskDeclaration::createTask() {
+  addLayer(new TaskFile());
+}
+
+void TaskDeclaration::unloadFiles() {
+  foreach(TaskLayer *layer, layers) {
+    delete layer;
+  }
+  layers.clear();
+}
+
+void TaskDeclaration::addLayer(TaskFile *file) {
+  if (file == NULL) {
+    return;
+  }
+
+  // Create and add new layer item, build scene.
+  layers.append(new TaskLayer(true, this, file, getIndexForNewTask()));
+}
+
+int TaskDeclaration::getIndexForNewTask() {
+  int maxIndex = 0;
+
+  // Finds maximal index of unstored new tasks.
+  foreach(TaskLayer *layer, layers) {
+    switch (layer->getStorageState()) {
+      case TaskFile::UNSTORED_EMPTY:
+      case TaskFile::UNSTORED_EDITED:
+        maxIndex = qMax(layer->getNewTaskIndex(), maxIndex);
+        break;
+    }
+  }
+
+  return maxIndex + 1;
 }
 
 Q_EXPORT_PLUGIN2(taskdecl, TaskDeclaration)
 
-}  // End namespace Core
 }  // End namespace Updraft
-
---
+++ Updraft/src/plugins/taskdecl/tasklayer.cpp
@@ -0,0 +1,103 @@
+#include <osg/Group>
+#include "tasklayer.h"
+#include "coreinterface.h"
+#include "tabinterface.h"
+#include "taskdeclpanel.h"
+#include "taskdeclaration.h"
+
+namespace Updraft {
+
+TaskLayer::TaskLayer(bool displayed_, TaskDeclaration *plugin_,
+  TaskFile *file_,
+  int _newTaskIndex)
+  : group(new osg::Group()),
+  plugin(plugin_),
+  mapLayer(NULL),
+  file(file_),
+  displayed(displayed_),
+  newTaskIndex(_newTaskIndex) {
+  // Create new tab in bottom pane.
+  tab = plugin->core->createTab(new TaskDeclPanel(), getTitle());
+
+  // Create new mapLayer in mapLayerGroup, assign osgNode and title.
+  mapLayer = plugin->mapLayerGroup->insertMapLayer(getNode(), getTitle(), -1);
+
+  mapLayer->connectSignalDisplayed(this,
+    SLOT(mapLayerDisplayed(bool, MapLayerInterface*)));
+
+  tab->connectSignalCloseRequested(this, SLOT(tryCloseLayer()));
+}
+
+TaskLayer::~TaskLayer() {
+  plugin->mapLayerGroup->removeMapLayer(mapLayer);
+  tab->close();
+  delete file;
+}
+
+osg::Node* TaskLayer::getNode() const {
+  return group;
+}
+
+bool TaskLayer::isDisplayed() {
+  return displayed;
+}
+
+void TaskLayer::display(bool displayed_) {
+  displayed = displayed_;
+}
+
+QString TaskLayer::getTitle() const {
+  switch (file->getStorageState()) {
+    case TaskFile::UNSTORED_EMPTY:
+      return QString("[New Task %1]").arg(newTaskIndex);
+
+    case TaskFile::UNSTORED_EDITED:
+      return QString("[New Task %1] *").arg(newTaskIndex);
+
+    case TaskFile::STORED_SYNCHRONIZED:
+      return file->getFileName();
+
+    case TaskFile::STORED_DIRTY:
+      return file->getFileName() + " *";
+
+    default:
+      return "";
+  }
+}
+
+TaskFile::StorageState TaskLayer::getStorageState() const {
+  return file->getStorageState();
+}
+
+int TaskLayer::getNewTaskIndex() const {
+  return newTaskIndex;
+}
+
+void TaskLayer::mapLayerDisplayed(bool value, MapLayerInterface* sender) {
+  if (sender != mapLayer) {
+    return;
+  }
+
+  // First try to display/enable layer in TaskDeclaration plugin.
+  // Then if it is displayed, show it also in the map.
+  display(value);
+  mapLayer->setVisible(isDisplayed());
+}
+
+void TaskLayer::tryCloseLayer() {
+  // TODO(Tom): Ask for close, allow refuse.
+
+  // Removes layer from list in plugin.
+  TTaskLayerList::iterator itLayer = plugin->layers.begin();
+  for (; itLayer != plugin->layers.end(); ++itLayer) {
+    if ((*itLayer) == this) {
+      break;
+    }
+  }
+  if (itLayer != plugin->layers.end())
+    plugin->layers.erase(itLayer);
+
+  delete this;
+}
+
+}  // End namespace Updraft
