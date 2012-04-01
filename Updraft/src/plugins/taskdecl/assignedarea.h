aabfcf8ce4d6b1662c21fb60450d46e600daa7a2
Tomas Zamecnik <pulcik@email.cz>
2012-04-01 15:55:33 +0200

    Create AssignedArea structure
    * A structure for storing task-point's area shape

---
+++ Updraft/src/plugins/taskdecl/assignedarea.h
@@ -0,0 +1,61 @@
+#ifndef UPDRAFT_SRC_PLUGINS_TASKDECL_ASSIGNEDAREA_H_
+#define UPDRAFT_SRC_PLUGINS_TASKDECL_ASSIGNEDAREA_H_
+
+namespace Updraft {
+
+/// A structure defining area shape
+/// Shape styles are same as in SeeYou 4.2 by NavIter
+struct AssignedArea {
+  /// Assigned area shapes.
+  /// Other shapes like cylinder or FAI cylinder are special
+  /// cases of SECTOR shape.
+  /// E.g. FAI cylinder has maxRadius=0.5 outerAngle=180
+  /// and other values set to 0.
+  enum AreaShape {
+    LINE,
+    SECTOR
+  };
+
+  /// Definition of base direction with respect to two neighbour points
+  /// or start point or fixed value.
+  enum Direction {
+    FIXED,
+    SYMMETRICAL,
+    TO_NEXT_POINT,
+    TO_PREVIOUS_POINT,
+    TO_START_POINT
+  };
+
+  /// Area shape type
+  AreaShape areaShape;
+
+  /// Base direction of the area
+  Direction direction;
+
+  /// Radius defining outer circle (maximum distance)
+  /// Unit is km
+  double maxRadius;
+
+  /// Radius defining inner circle (minimal distance)
+  /// Unit is km
+  double minRadius;
+
+  /// Angle from base direction to left and right,
+  /// defining the range of outer sector. (range is 2*outerAngle)
+  double outerAngle;
+
+  /// Angle from base direction to left and right,
+  /// defining the range of inner sector. (range is 2*innerAngle)
+  double innerAngle;
+
+  /// Angle which is used when the direction is FIXED
+  double fixedAngle;
+
+  /// Default constructor sets area to FAI cylinder.
+  AssignedArea() : areaShape(SECTOR), direction(SYMMETRICAL), maxRadius(0.5),
+    minRadius(0.0), outerAngle(180.0), innerAngle(0.0), fixedAngle(0.0) { }
+};
+
+}  // End namespace Updraft
+
+#endif  // UPDRAFT_SRC_PLUGINS_TASKDECL_ASSIGNEDAREA_H_
