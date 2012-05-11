07c32dac5e0e7bf6a721eac1867bc26123a0c151
Kuba Marek <blue.cube@seznam.cz>
2012-05-12 01:38:18 +0200

    Czech translation

---
+++ Updraft/src/core/translations/cs_cz.ts
@@ -0,0 +1,163 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="cs">
+<context>
+    <name>FileRolesDialog</name>
+    <message>
+        <location filename="../ui/filerolesdialog.ui" line="14"/>
+        <source>Open</source>
+        <translation type="unfinished">Otevřít</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="20"/>
+        <source>Updraft</source>
+        <translation>Updraft</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="47"/>
+        <source>Displayed Layers</source>
+        <translation type="unfinished">Zobrazené vrstvy</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="125"/>
+        <source>&amp;File</source>
+        <translation>&amp;Soubor</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="130"/>
+        <source>&amp;View</source>
+        <translation>&amp;Zobrazit</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="135"/>
+        <source>&amp;Tools</source>
+        <translation>&amp;Nástroje</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="140"/>
+        <source>&amp;Help</source>
+        <translation>Nápo&amp;věda</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../ui/settingsdialog.ui" line="14"/>
+        <source>Options</source>
+        <translation type="unfinished">Možnosti</translation>
+    </message>
+    <message>
+        <location filename="../ui/settingsdialog.ui" line="103"/>
+        <source>Show advanced options</source>
+        <translation type="unfinished">Zobrazit pokročilé možnosti</translation>
+    </message>
+</context>
+<context>
+    <name>Updraft</name>
+    <message>
+        <location filename="../updraft.cpp" line="32"/>
+        <source>WGS84</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../updraft.cpp" line="34"/>
+        <source>FAI Sphere</source>
+        <translation type="unfinished">FAI Koule</translation>
+    </message>
+</context>
+<context>
+    <name>Updraft::Core::FileOpenDialog</name>
+    <message>
+        <location filename="../ui/fileopendialog.cpp" line="53"/>
+        <source>All supported types</source>
+        <translation type="unfinished">Všechny typy</translation>
+    </message>
+</context>
+<context>
+    <name>Updraft::Core::MainWindow</name>
+    <message>
+        <location filename="../ui/mainwindow.cpp" line="141"/>
+        <source>&amp;Open...</source>
+        <translation type="unfinished">&amp;Otevřít...</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.cpp" line="145"/>
+        <source>E&amp;xit</source>
+        <translation type="unfinished">&amp;Konec</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.cpp" line="155"/>
+        <source>Updraft Help</source>
+        <translation type="unfinished">Témata nápovědy</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.cpp" line="159"/>
+        <source>&amp;About...</source>
+        <translation type="unfinished">&amp;O programu...</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.cpp" line="169"/>
+        <source>Open</source>
+        <translation type="unfinished">Otevřít</translation>
+    </message>
+</context>
+<context>
+    <name>Updraft::Core::SceneManager</name>
+    <message>
+        <location filename="../scenemanager.cpp" line="101"/>
+        <source>Rotate to north</source>
+        <translation type="unfinished">Otočit na sever</translation>
+    </message>
+    <message>
+        <location filename="../scenemanager.cpp" line="105"/>
+        <source>Restore 2D View</source>
+        <translation type="unfinished">Obnovit 2D zobrazení</translation>
+    </message>
+</context>
+<context>
+    <name>Updraft::Core::SettingsDialog</name>
+    <message>
+        <source>Settings</source>
+        <translation type="obsolete">Možnosti</translation>
+    </message>
+    <message>
+        <location filename="../ui/settingsdialog.cpp" line="63"/>
+        <source>Reset to defaults?</source>
+        <translation type="unfinished">Obnovit výchozíí?</translation>
+    </message>
+    <message>
+        <location filename="../ui/settingsdialog.cpp" line="64"/>
+        <source>All settings values will be erased and set to defaults. Are you sure you want to continue?</source>
+        <translation type="unfinished">Všechny možnosti budou vráceny na výchozí hodnoty. Chcete pokračovat?</translation>
+    </message>
+</context>
+<context>
+    <name>Updraft::Core::SettingsManager</name>
+    <message>
+        <location filename="../settingsmanager.cpp" line="33"/>
+        <source>Settings...</source>
+        <translation type="unfinished">Možnosti...</translation>
+    </message>
+    <message>
+        <location filename="../settingsmanager.cpp" line="116"/>
+        <source> (hidden)</source>
+        <translation type="unfinished">(skryté)</translation>
+    </message>
+</context>
+<context>
+    <name>Updraft::Core::SettingsTopView</name>
+    <message>
+        <location filename="../ui/settingstopview.cpp" line="82"/>
+        <source>Unsaved changes in this tab.</source>
+        <translation type="unfinished">Neuložené změny v této záložce.</translation>
+    </message>
+    <message>
+        <location filename="../ui/settingstopview.cpp" line="83"/>
+        <source>You have some unsaved changes to the setting values in the current settings group. Do you want to apply these changes or reset the settings to their previous values?</source>
+        <translation type="unfinished">V této záložce jsou neuložené změny. Chcete tyto změny použít, nebo obnovit předchozí hodnoty?</translation>
+    </message>
+</context>
+</TS>
