0ef406f53624075608fd3e66a1de3bd26000e015
Cestmir Houska <czestmyr@gmail.com>
2012-05-24 13:52:47 +0200

    Added mouse zoom sensitivity to settings
    Signed-off-by: Cestmir Houska <czestmyr@gmail.com>

--- Updraft/src/core/mapmanipulator.h
+++ Updraft/src/core/mapmanipulator.h
@@ -2,15 +2,27 @@
 #define UPDRAFT_SRC_CORE_MAPMANIPULATOR_H_
 
 #include <osgEarthUtil/EarthManipulator>
+#include <QObject>
 
 namespace Updraft {
+
+class SettingInterface;
+
 namespace Core {
 
-class MapManipulator: public osgEarth::Util::EarthManipulator {
+class MapManipulator: public QObject, public osgEarth::Util::EarthManipulator {
+  Q_OBJECT
+
  public:
   MapManipulator();
   void resetNorth(double duration);
   void untilt(double duration);
+
+ private slots:
+  void mouseZoomSensitivityChanged();
+
+ private:
+  SettingInterface* mouseZoomSensitivity;
 };
 
 }  // End namespace Core
