5e9eb73da52bbab7d593b290ca5260565c0d4be1
Tomas Zamecnik <pulcik@email.cz>
2012-06-04 12:48:12 +0200

    Correct labels for igc statistics

--- Updraft/src/plugins/igcviewer/plotwidget.cpp
+++ Updraft/src/plugins/igcviewer/plotwidget.cpp
@@ -356,12 +356,12 @@ QString PlotWidget::createSegmentStatText(
   avgspeedstr.setNum(avgSpeed, 5, 0);
   QString avgrisestr;
   avgrisestr.setNum(avgRise, 5, 1);
-  text = tr("dT:") + " "
+  text = "dT: "
     + durationhrs + ":" + durationmins + ":" + durationsecs + "\n"
-    + tr("d:") + " " + distancestr + " km\n"
-    + tr("dH") + " " + heightstr + " m\n"
-    + tr("GS") + " " + avgspeedstr + " km/h\n"
-    + tr("VS") + " " + avgrisestr + " m/s";
+    + "dS: " + distancestr + " km\n"
+    + "dH: " + heightstr + " m\n"
+    + "GS: " + avgspeedstr + " km/h\n"
+    + "VS: " + avgrisestr + " m/s";
   return text;
 }
 
