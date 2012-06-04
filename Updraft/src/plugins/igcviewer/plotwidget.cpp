ebd41a57c49bb9c83af6b134178b950af6c9de51
maria <maria.vamosova@gmail.com>
2012-06-04 14:25:38 +0200

    Bug fix.

--- Updraft/src/plugins/igcviewer/plotwidget.cpp
+++ Updraft/src/plugins/igcviewer/plotwidget.cpp
@@ -271,8 +271,8 @@ void PlotWidget::updatePickedTexts(int i) {
 void PlotWidget::resetStats() {
   pickedFixesStatTexts.clear();
   pickedPositions.clear();
+  pickedFixes.clear();
   segmentsStatTexts.clear();
-  pickedFixesStatTexts.clear();
 
   PickData first(qMax(0, altitudePlotPainter->getMinX()), 0);
   PickData last(qMax(0, altitudePlotPainter->getMaxX()),
