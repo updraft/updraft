ff7c6cdfbe21f1512b444f4e3bc49b9d5f6ba639
Bohdan <bohdan.maslowski@gmail.com>
2012-06-06 14:00:31 +0200

    Osg build scripts and patch for mac

--- SourcesInstallation/mac/build-osgearth.sh
+++ SourcesInstallation/mac/build-osgearth.sh
@@ -1,10 +1,10 @@
 rm CMakeCache.txt
 
-DESKTOP=/Users/User/Desktop
-export OSG_DIR=$DESKTOP/osg_release
+INSTALL_DIR=/Users/User/Desktop/osgearth_release
+export OSG_DIR=/Users/User/Desktop/osg_release
 export DYLD_LIBRARY_PATH=$OSG_DIR/lib
 
-cmake . -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES=i386 -DOSG_VERSION_EXE=$OSG_DIR/bin/osgversion -DCMAKE_INSTALL_PREFIX=$DESKTOP/osgearth_release -DOSG_DIR=$OSG_DIR
+cmake . -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES=i386 -DOSG_VERSION_EXE=$OSG_DIR/bin/osgversion -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DOSG_DIR=$OSG_DIR
 
 make
 make install
---
+++ SourcesInstallation/mac/osg_patch/readme.txt
@@ -0,0 +1,5 @@
+Corrected handling of 16 bit tiff images (required for heightmaps)
+
+Instructions:
+
+replace src/osgPlugins/imageio/ReaderWriterImageIO.cpp with included file. 
