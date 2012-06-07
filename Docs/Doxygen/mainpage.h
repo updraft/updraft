82f86b44b5707626a078da9c3de754c19c5c4d5b
Kuba Marek <blue.cube@seznam.cz>
2012-06-07 06:10:23 +0200

    Pimp my doxygen.

---
+++ Docs/Doxygen/mainpage.h
@@ -0,0 +1,15 @@
+/**
+
+\mainpage
+
+Updraft is an oppen source project that aims to create user-friendly application for
+glider flight planning and visualization.
+
+Features include visualization of 3D maps, flight planning and
+visualizations of recorded IGC files, all of that on Windows, Linux and Mac OS X.
+
+- Project website: http://updraft.github.com/
+- Git repository: http://github.com/updraft/updraft/
+- E-mail: glideplan_swproj@freelists.org
+
+*/
